f46c8f94f08149b76c32ccd5bd185cbf4ce6dde8
Yusuf <yusufafzal12@gmail.com>
2025-08-16 22:17:53 -0700

    Remove uuid package and more formatting

--- service/lambdas/createTeam/src/createTeam.ts
+++ service/lambdas/createTeam/src/createTeam.ts
@@ -1,11 +1,11 @@
 import {
-    APIGatewayProxyEventV2WithLambdaAuthorizer,
-    APIGatewayProxyResultV2,
-    Handler,
+	APIGatewayProxyEventV2WithLambdaAuthorizer,
+	APIGatewayProxyResultV2,
+	Handler,
 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
-import { v4 as uuidv4 } from "uuid";
+import { randomUUID } from "crypto";
 import { AuthorizerContext } from "models/auth";
 import { removeKeys } from "resources/dynamo/utilities";
 
@@ -15,58 +15,58 @@ const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler: Handler = async (
-    event: APIGatewayProxyEventV2WithLambdaAuthorizer<AuthorizerContext>,
-    context
+	event: APIGatewayProxyEventV2WithLambdaAuthorizer<AuthorizerContext>,
+	context,
 ): Promise<APIGatewayProxyResultV2> => {
-    console.log(JSON.stringify({ event, context }, null, 4));
+	console.log(JSON.stringify({ event, context }, null, 4));
 
-    const { sub: userUUID, username } = event.requestContext.authorizer.lambda;
+	const { sub: userUUID, username } = event.requestContext.authorizer.lambda;
 
-    try {
-        const teamUUID = uuidv4();
-        const timestamp = new Date().getTime();
-        const initialMetadata = {
-            publiclyViewable: false,
-        };
-        const initialTeam = {
-            PK: `userUUID#${userUUID}`,
-            SK: `team#${teamUUID}`,
-            createdAt: timestamp,
-            description: "",
-            favorited: false,
-            label: "",
-            lastViewed: timestamp,
-            metadata: initialMetadata,
-            updatedAt: timestamp,
-            teamUUID,
-            title: "Untitled Team",
-            username,
-            userUUID,
-        };
+	try {
+		const teamUUID = randomUUID();
+		const timestamp = new Date().getTime();
+		const initialMetadata = {
+			publiclyViewable: false,
+		};
+		const initialTeam = {
+			PK: `userUUID#${userUUID}`,
+			SK: `team#${teamUUID}`,
+			createdAt: timestamp,
+			description: "",
+			favorited: false,
+			label: "",
+			lastViewed: timestamp,
+			metadata: initialMetadata,
+			updatedAt: timestamp,
+			teamUUID,
+			title: "Untitled Team",
+			username,
+			userUUID,
+		};
 
-        const putCommand = new PutCommand({
-            TableName: mainTable,
-            Item: initialTeam,
-        });
+		const putCommand = new PutCommand({
+			TableName: mainTable,
+			Item: initialTeam,
+		});
 
-        await docClient.send(putCommand);
+		await docClient.send(putCommand);
 
-        removeKeys(initialTeam);
+		removeKeys(initialTeam);
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "Successfully created team",
-                team: initialTeam,
-            }),
-        };
-    } catch (err) {
-        console.error(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: "Error",
-            }),
-        };
-    }
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: "Successfully created team",
+				team: initialTeam,
+			}),
+		};
+	} catch (err) {
+		console.error(err);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				message: "Error",
+			}),
+		};
+	}
 };
